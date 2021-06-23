dbd12ea00fbd0ec63d01a951dafb9af0ccd1467c
hyeran <82014212+hyeran930331@users.noreply.github.com>
2021-06-23 17:53:44 +0900

    feed 고치기
    feed로 이름 쓰기

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,7 +67,7 @@ jQuery(document).ready(function($) {
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "hyeran930331", selector: "feed" });
 
 
 });
